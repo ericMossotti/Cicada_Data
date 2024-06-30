c60652a50ead4ac1e06f19b7a9b1a6ade8f110a3
Eric Mossotti <ecmossotti@gmail.com>
2024-06-30 02:03:33 -0500

    Remove unused code

--- 00_Extract/scripts/importer.py
+++ 00_Extract/scripts/importer.py
@@ -1,20 +1,9 @@
 # ---- Import data from the web with wget
-
 import os
 import sys
 import wget
 
 def importer (fileMap):
-    
-    # Check for directory, then skip, or create dir and import files
-    #if not os.path.exists(directory):
-    #    os.makedirs(directory)
-    #    print(f"Directory {directory} created successfully.")
-        
-    # Otherwise, notify if exists
-    #else:
-    #    print(f"Directory {directory} already exists.")
-        
     # Download from URL to path and notify when complete
     for url, file_path in fileMap.items():
         # Check if file exists before downloading
