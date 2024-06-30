f9bca54e4d42f526d13a1cd9e26f0849a5af99ec
Eric Mossotti <ecmossotti@gmail.com>
2024-06-30 02:04:21 -0500

    Update wording

--- 02_Load/scripts/strint.py
+++ 02_Load/scripts/strint.py
@@ -6,8 +6,8 @@ as a comma separated string
 import pandas as pd
 
 # Format output with comma seperator for thousands place
-def strint (df, category, label):
-    stat = df.loc[(category, label), "Value"]
+def strint (dataframe, category, label):
+    stat = dataframe.loc[(category, label), "Value"]
     stat = pd.to_numeric(stat, downcast = "integer")
     stat = f'{stat:,}'
     
