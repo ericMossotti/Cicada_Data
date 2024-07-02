c88685799a2520ea4be536a13ce60173e6e765af
Eric Mossotti <ecmossotti@gmail.com>
2024-07-01 22:53:19 -0500

    Update for handling integers and floats

--- 02_Load/scripts/strint.py
+++ 02_Load/scripts/strint.py
@@ -4,11 +4,24 @@ as a comma separated string
 """
 
 import pandas as pd
+import re
 
 # Format output with comma seperator for thousands place
 def strint (dataframe, category, label):
+    
+    # Find the desired value
     stat = dataframe.loc[(category, label), "Value"]
-    stat = pd.to_numeric(stat, downcast = "integer")
+    # Set boolean match value
+    isFloat = re.search(r"\.", stat)
+    
+    # Convert to float if there is a decimal
+    if isFloat:
+        stat = pd.to_numeric(stat, downcast = "float")
+    else:
+        # Else convert to an integer 
+        stat = pd.to_numeric(stat, downcast = "integer")
+        
+    # Add a thousands seperator and convert back to a string
     stat = f'{stat:,}'
     
     return stat 
